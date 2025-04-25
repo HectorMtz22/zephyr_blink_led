ac7b21f9a7ac04747ad36773c037d6e88d6af28b
Hector Mtz <hec.gre81@gmail.com>
2025-04-25 01:53:40 -0600

    Change blinking to internal pin on esp32

--- src/main.c
+++ src/main.c
@@ -3,20 +3,21 @@
 #include <zephyr/drivers/gpio.h>
 
 #define DELAY_TIME 200
+#define LED_PIN 2
 
 static const struct device *gpio0 =
 	DEVICE_DT_GET(DT_NODELABEL(gpio0));
 
 int main(void)
 {
-	gpio_pin_configure(gpio0, 25, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_configure(gpio0, LED_PIN, GPIO_OUTPUT_ACTIVE);
 
 	while (true)
 	{
-		gpio_pin_set_raw(gpio0, 25, 1);
+		gpio_pin_set_raw(gpio0, LED_PIN, 1);
 		k_msleep(DELAY_TIME);
 
-		gpio_pin_set_raw(gpio0, 25, 0);
+		gpio_pin_set_raw(gpio0, LED_PIN, 0);
 		k_msleep(DELAY_TIME);
 	}
 	return 0;
